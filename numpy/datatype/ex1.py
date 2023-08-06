7de687c41f358ab4bbb2e784fb611e25a4711249
Lee Hyun Ho <118269278+hyunho4532@users.noreply.github.com>
2023-08-06 15:31:44 +0000

    2023-08-07 numpy 생성

---
+++ numpy/datatype/ex1.py
@@ -0,0 +1,28 @@
+# ndarray의 데이터 타입
+import numpy as np
+
+list1 = [1, 2, 3]
+
+array1 = np.array(list1)
+print(type(array1))
+print(array1, array1.dtype)
+
+list2 = [1, 2, 'test']
+
+array2 = np.array(list2)
+print(array2, array2.dtype)
+
+list3 = [1, 2, 3.0]
+array3 = np.array(list3)
+print(array3, array3.dtype)
+
+array_int = np.array([1, 2, 3])
+array_float = array_int.astype('float64')
+print(array_float, array_float.dtype)
+
+array_int1 = array_float.astype('int32')
+print(array_int1, array_int1.dtype)
+
+array_float1 = np.array([1.1, 2.1, 3.1])
+array_int2 = array_float1.astype('int32')
+print(array_int2, array_int2.dtype)
---
+++ numpy/ex1.py
@@ -0,0 +1,23 @@
+# 넘파이 모듈 임포트
+
+import numpy as np
+
+
+# 1차원 배열
+array1 = np.array([1, 2, 3])
+
+print('array1.type:', type(array1))
+print('array1 array 형태:', array1.shape)
+
+# 2차원 배열
+array2 = np.array([[1, 2, 3], [4, 5, 6]])
+
+print('array2.type:', type(array2))
+print('array2 array 형태:', array2.shape)
+
+array3 = np.array([[1, 2, 3]])
+print('array3 type:', type(array3))
+print('array3 array 형태:', array3.shape)
+
+
+
