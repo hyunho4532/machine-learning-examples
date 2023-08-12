dbe233a01cd8fae3ea7d0796ebf17091e21b180b
Lee Hyun Ho <118269278+hyunho4532@users.noreply.github.com>
2023-08-12 10:44:35 +0000

    2023-08-12 인덱싱, 슬라이싱 1차

---
+++ numpy/chapter01/indexing/ex1.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+array1 = np.arange(start=1, stop=10)
+print('array1: ', array1)
+
+value = array1[2]
+print('value: ', value)
+print(type(value))
+
+print('맨 뒤의 값:', array1[-1], '맨 뒤에서 두 번째 값:', array1[-2])
+
+array1[0] = 9
+array1[8] = 0
+print('array1:', array1)
+
+array1d = np.arange(start=1, stop=10)
+array2d = array1d.reshape(3, 3)
+print(array2d)
+
+print('(row=0, col=0) index 가리키는 값:', array2d[0, 0])
+print('(row=1, col=1) index 가리키는 값:', array2d[1, 1])
+print('(row=1, col=0) index 가리키는 값:', array2d[1, 0])
+print('(row=2, col=2) index 가리키는 값:', array2d[2, 2])
+
+array1 = np.arange(start=1, stop=10)
+array3 = array1[0:3]
+print(array3)
+print(type(array3))
+
+array1 = np.arange(start=1, stop=10)
+array4 = array1[:3]
+print(array4)
+
+array5 = array1[:5]
+print(array5)
+
+array6 = array1[-2:]
+print(array6)
+
